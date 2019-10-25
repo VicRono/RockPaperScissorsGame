8c30dfcc18f1ef346d89b9efa92bbca53a1fa0f3
Victor Rono <v.rono@icloud.com>
2019-10-25 15:58:40 -0500

    added scoreboard object

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
+// DOM elements
 const choices = document.querySelectorAll('.choice');
 const score = document.getElementById('score');
 const result = document.getElementById('result');
 const restart = document.getElementById('restart');
 const modal = document.querySelector('.modal');
+
+// scoreboard object
+const scoreboard = {
+  player : 0,
+  computer : 0
+}

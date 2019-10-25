c424e048696f85634b5d931d76aa0c53242acadf
Victor Rono <v.rono@icloud.com>
2019-10-24 19:12:58 -0500

    used 'getElementById' since restart is captured as an id

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 const choices = document.querySelectorAll('.choice');
 const score = document.getElementById('score');
 const result = document.getElementById('result');
+const restart = document.getElementById('restart');
